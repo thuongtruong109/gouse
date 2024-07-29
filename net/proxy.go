767c848dec59b0c63a2e9ea37b9786419bcdb95d
truongtop1 <pattlactroi@gmail.com>
2024-07-29 09:09:31 +0700

    feat(net): add forward proxy function

--- net/proxy.go
+++ net/proxy.go
@@ -1,79 +1,78 @@
 package net
 
 import (
+	"fmt"
 	"io"
 	"net/http"
-	"strings"
+	"sync/atomic"
 
 	"github.com/gin-gonic/gin"
 )
 
-func handleRequest(ctx *gin.Context) {
-	path := ctx.Param("path")
+var counter uint64
 
-	if path == "/" {
-		ctx.IndentedJSON(http.StatusOK, gin.H{
-			"message": "CORS Proxy. Just go to /:url to use",
-		})
-		ctx.Done()
-		return
+func handleRequest(urls []string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		path := ctx.Param("path")
+		if path == "" {
+			ctx.IndentedJSON(http.StatusBadRequest, gin.H{
+				"message": "Path is required",
+			})
+			ctx.Done()
+			return
+		}
+
+		index := atomic.AddUint64(&counter, 1) % uint64(len(urls))
+		requestedURL := urls[index] + path[1:]
+
+		fmt.Println("Requested URL: ", requestedURL)
+
+		req, _ := http.NewRequest(ctx.Request.Method, requestedURL, ctx.Request.Body)
+
+		req.Header = ctx.Request.Header.Clone()
+		req.Header.Del("origin")
+		req.Header.Del("referer")
+
+		queryValues := req.URL.Query()
+		for k, v := range ctx.Request.URL.Query() {
+			queryValues.Add(k, v[0])
+		}
+		req.URL.RawQuery = queryValues.Encode()
+
+		response, err1 := http.DefaultClient.Do(req)
+
+		for k, v := range response.Header.Clone() {
+			ctx.Header(k, v[0])
+		}
+
+		ctx.Header("Access-Control-Allow-Origin", "*")
+		ctx.Header("Access-Control-Allow-Methods", "*")
+		ctx.Header("Access-Control-Allow-Headers", "*")
+
+		responseBytes, err2 := io.ReadAll(response.Body)
+
+		if err1 != nil || err2 != nil {
+			ctx.IndentedJSON(http.StatusInternalServerError, gin.H{
+				"message": "Failed to request",
+			})
+			ctx.Done()
+			return
+		}
+
+		ctx.Data(response.StatusCode, response.Header.Get("Content-Type"), responseBytes)
 	}
-
-	if !strings.HasPrefix(path, "/http") {
-		ctx.IndentedJSON(http.StatusBadRequest, gin.H{
-			"message": "Invalid URL",
-		})
-		ctx.Done()
-		return
-	}
-
-	requestedURL := path[1:]
-
-	req, _ := http.NewRequest(ctx.Request.Method, requestedURL, ctx.Request.Body)
-
-	req.Header = ctx.Request.Header.Clone()
-	req.Header.Del("origin")
-	req.Header.Del("referer")
-
-	queryValues := req.URL.Query()
-	for k, v := range ctx.Request.URL.Query() {
-		queryValues.Add(k, v[0])
-	}
-	req.URL.RawQuery = queryValues.Encode()
-
-	response, err1 := http.DefaultClient.Do(req)
-
-	for k, v := range response.Header.Clone() {
-		ctx.Header(k, v[0])
-	}
-
-	ctx.Header("Access-Control-Allow-Origin", "*")
-	ctx.Header("Access-Control-Allow-Methods", "*")
-	ctx.Header("Access-Control-Allow-Headers", "*")
-
-	responseBytes, err2 := io.ReadAll(response.Body)
-
-	if err1 != nil || err2 != nil {
-		ctx.IndentedJSON(http.StatusInternalServerError, gin.H{
-			"message": "Failed to request",
-		})
-		ctx.Done()
-		return
-	}
-
-	ctx.Data(response.StatusCode, response.Header.Get("Content-Type"), responseBytes)
 }
 
-func Proxy(port string) {
+func Proxy(port string, urls []string) {
 	router := gin.Default()
 
-	router.GET("*path", handleRequest)
-	router.POST("*path", handleRequest)
-	router.PUT("*path", handleRequest)
-	router.PATCH("*path", handleRequest)
-	router.DELETE("*path", handleRequest)
-	router.OPTIONS("*path", handleRequest)
-	router.HEAD("*path", handleRequest)
+	router.GET("*path", handleRequest(urls))
+	router.POST("*path", handleRequest(urls))
+	router.PUT("*path", handleRequest(urls))
+	router.PATCH("*path", handleRequest(urls))
+	router.DELETE("*path", handleRequest(urls))
+	router.OPTIONS("*path", handleRequest(urls))
+	router.HEAD("*path", handleRequest(urls))
 
 	router.Run(":" + port)
 }
