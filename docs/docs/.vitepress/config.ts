5d36631ffbb5075eb0648dde1330d9974f10e99e
truongtop1 <pattlactroi@gmail.com>
2024-11-25 00:27:44 +0700

    docs: update document web

--- docs/docs/.vitepress/config.ts
+++ docs/docs/.vitepress/config.ts
@@ -1,42 +1,130 @@
 import { defineConfig } from 'vitepress';
+import { createRequire } from 'module'
 
-// refer https://vitepress.dev/reference/site-config for details
+const require = createRequire(import.meta.url)
+const pkg = require('../../package.json')
+
 export default defineConfig({
   lang: 'en-US',
-  title: 'VitePress',
-  description: 'Vite & Vue powered static site generator.',
+  title: 'Gouse',
+  description: 'A modern essential Golang utility package delivering consistency, modularity, performance, & extras presets',
+
+
+  sitemap: {
+    hostname: 'http://localhost:3000',
+  },
+
+  head: [
+    [
+      'script',
+      { id: 'register-sw' },
+      `;(() => {
+        if ('serviceWorker' in navigator) {
+          navigator.serviceWorker.register('/sw.js')
+        }
+      })()`
+    ],
+    [
+      'favicon',
+      { rel: 'icon', type: 'image/ico', href: '/img/favicon.ico' }
+    ]
+  ],
 
   themeConfig: {
     nav: [
-      { text: 'Example', link: '/example' },
-
       {
-        text: 'Dropdown Menu',
-        items: [
-          { text: 'Item A', link: '/item-1' },
-          { text: 'Item B', link: '/item-2' },
-          { text: 'Item C', link: '/item-3' },
-        ],
-      },
-
-      // ...
-    ],
-
-    sidebar: [
-      {
-        text: 'Guide',
-        items: [
-          { text: 'Example', link: '/example' },
-          // ...
-        ],
+        text: 'Introduction',
+        link: '/introduction/what-is-gouse',
+        activeMatch: '/introduction/'
       },
       {
         text: 'Receipts',
-        items: [
-          { text: 'Receipt', link: '/example' },
-          // ...
-        ],
+        link: '/receipts/array',
+        activeMatch: '/receipts/'
       },
+      {
+        text: `v${pkg.version}`,
+        items: [
+          {
+            text: 'Changelog',
+            link: 'https://github.com/thuongtruong109/gouse/blob/main/CHANGELOG.md'
+          },
+          {
+            text: 'Contributing',
+            link: 'https://github.com/thuongtruong109/gouse/blob/main/.github/CONTRIBUTING.md'
+          }
+        ]
+      }
     ],
+
+    sidebar: {
+      '/introduction/': { 
+        base: '/introduction/', 
+        items: [
+            { text: 'What is Gouse?', link: 'what-is-gouse' },
+            { text: 'Getting Started', link: 'getting-started' },
+          ]
+        
+      },
+      '/receipts/': { 
+        base: '/receipts/', 
+        items: [
+          { text: 'Api', link: 'api' },
+          { text: 'Array', link: 'array' },
+          { text: 'Cache', link: 'cache' },
+          { text: 'Chart', link: 'chart' },
+          { text: 'Config', link: 'config' },
+          { text: 'Connection', link: 'connection' },
+          { text: 'Console', link: 'console' },
+          { text: 'Cron', link: 'cron' },
+          { text: 'Crypto', link: 'crypto' },
+          { text: 'Date', link: 'date' },
+          { text: 'Function', link: 'function' },
+          { text: 'I/O', link: 'io' },
+          { text: 'Log', link: 'Log' },
+          { text: 'Math', link: 'math' },
+          { text: 'Media', link: 'media' },
+          { text: 'Net', link: 'net' },
+          { text: 'Number', link: 'number' },
+          { text: 'OS', link: 'os' },
+          { text: 'Random', link: 'random' },
+          { text: 'Regex', link: 'regex' },
+          { text: 'String', link: 'string' },
+          { text: 'Struct', link: 'struct' },
+          { text: 'Type', link: 'type' },
+
+          // { text: 'Path', link: 'path' },
+          // { text: 'Security', link: 'security' },
+          // { text: 'Web', link: 'web' },
+          // { text: 'Worker', link: 'worker' },
+          // { text: 'XML', link: 'xml' },
+          // { text: 'Zip', link: 'zip' },
+          // { text: 'Zlib', link: 'zlib' },
+          // { text: 'Collection', link: 'collection' },
+          // { text: 'Convert', link: 'convert' },
+          // { text: 'Debug', link: 'debug' },
+          // { text: 'File', link: 'file' },
+          // { text: 'Hash', link: 'hash' },
+          // { text: 'Http', link: 'http' },
+          // { text: 'Json', link: 'json' },
+          // { text: 'Object', link: 'object' },
+          // { text: 'Time', link: 'time' },
+          // { text: 'Uuid', link: 'uuid' },
+          // { text: 'Validate', link: 'validate' },
+          // { text: 'Web', link: 'web' },
+          // { text: 'Xml', link: 'xml' }
+        ]
+      },
+    },
+
+    editLink: {
+      pattern: 'https://github.com/thuongtruong109/gouse/edit/main/docs/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2024-present Tran Nguyen Thuong Truong'
+    }
   },
 });
